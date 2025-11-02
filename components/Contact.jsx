1e559f22955d223165a707dfe84ee452540f43da
ChiragPatankar <officialchiragp1605@gmail.com>
2025-11-02 13:05:48 +0530

    Update Twitter URL to https://x.com/_BuildVerse_

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -301,7 +301,7 @@ const Contact = () => {
                     <FiLinkedin className="w-5 h-5 text-slate-700 dark:text-white" />
                   </a>
                   <a
-                    href="https://twitter.com/buildverse"
+                    href="https://x.com/_BuildVerse_"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="w-10 h-10 rounded-lg bg-white/10 hover:bg-white/20 border border-white/20 flex items-center justify-center transition-all"
